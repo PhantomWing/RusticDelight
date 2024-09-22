45df1be6ff90aa8be07397718cf1a2bdbe7c03aa
Calvin Poleij <calvin@ichicraft.com>
2024-09-22 21:54:10 +0200

    Add empty 1.20.1 project

--- src/main/java/com/phantomwing/rusticdelight/RusticDelight.java
+++ src/main/java/com/phantomwing/rusticdelight/RusticDelight.java
@@ -1,69 +1,42 @@
 package com.phantomwing.rusticdelight;
 
-import com.phantomwing.rusticdelight.block.ModBlocks;
-import com.phantomwing.rusticdelight.loot.LootModifierManager;
-import com.phantomwing.rusticdelight.ui.ModCreativeModTab;
-import com.phantomwing.rusticdelight.item.ModItems;
+import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.FlowerPotBlock;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-import com.mojang.logging.LogUtils;
-
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
-
 @Mod(RusticDelight.MOD_ID)
-public class RusticDelight {
+public class RusticDelight
+{
     public static final String MOD_ID = "rusticdelight";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public RusticDelight(IEventBus eventBus, ModContainer modContainer) {
-        eventBus.addListener(this::commonSetup);
+    public RusticDelight()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        NeoForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::commonSetup);
 
-        registerManagers(eventBus);
-    }
-
-    // Register all managers to the event bus.
-    private void registerManagers(IEventBus eventBus) {
-        ModItems.register(eventBus);
-        ModBlocks.register(eventBus);
-        LootModifierManager.register(eventBus);
-        ModCreativeModTab.register(eventBus);
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-        event.enqueueWork(() -> {
-            addFlowerPots();
-        });
+
     }
 
-    private void addFlowerPots() {
-        FlowerPotBlock flowerPotBlock = ((FlowerPotBlock) Blocks.FLOWER_POT);
-        flowerPotBlock.addPlant(ModBlocks.WILD_COTTON.getId(), ModBlocks.POTTED_WILD_COTTON);
-        flowerPotBlock.addPlant(ModBlocks.WILD_BELL_PEPPERS.getId(), ModBlocks.POTTED_WILD_BELL_PEPPERS);
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-    }
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @EventBusSubscriber(modid = MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
         }
     }
 }
